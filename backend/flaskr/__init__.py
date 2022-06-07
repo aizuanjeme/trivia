f8184f43ca14628016efeca2fb2dfca44baf0561
ademuyiwa stephanie <stephanieademuyiwa1@gmail.com>
2022-06-07 11:42:58 +0100

    the addition of the cors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,16 +8,40 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    # CORS(app)
+    CORS(app, resources={r"*/api/*" : {"origins": '*'}})
+    
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
 
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
 
     """
+    
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
 
