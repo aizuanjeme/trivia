1f742658844348eb044419062f3d466bbfcbc330
ademuyiwa stephanie <stephanieademuyiwa1@gmail.com>
2022-06-14 16:58:21 +0100

    addition of rating

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
 
   render() {
       return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark ">
           <div className="navbar-brand" onClick={() => {this.navTo('')}}>Udacitrivia</div>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
